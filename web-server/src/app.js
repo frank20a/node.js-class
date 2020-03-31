37a279ea828d08a912dec9dfa9ceac6ac332730b
Frank Fourlas <frank1999ipod@gmail.com>
2020-03-31 20:57:30 +0300

    Section 8
    Finished section 7... moving on to section 8

--- web-server/src/app.js
+++ web-server/src/app.js
@@ -1,9 +1,15 @@
 const path = require('path')
 const express = require('express')
+const hbs = require('hbs')
 
 const app = express()
 
+//setup views engine and path
 app.set('view engine', 'hbs')
+app.set('views', path.join(__dirname, '../templates/views'))
+hbs.registerPartials(path.join(__dirname, '../templates/partials'))
+
+//setup static directory
 app.use(express.static(path.join(__dirname, '../public')))
 
 app.get('', (req, res) => {
@@ -22,7 +28,17 @@ app.get('/about', (req, res) => {
 
 app.get('/help', (req, res) => {
     res.render('help', {
-        msg: "This is a message by Frank. You're awesome!"
+        msg: "This is a message by Frank. You're awesome!",
+        title: "Help",
+        name: "Frank Fourlas"
+    })
+})
+
+app.get('/help/*', (req, res) => {
+    res.render('404', {
+        error: '404 - Help',
+        title: 'Help page not found',
+        name: 'Frank Fourlas'
     })
 })
 
@@ -30,7 +46,13 @@ app.get('/weather', (req, res) => {
     res.send({forecast: 'Clear', location: 'Athens'})
 })
 
-// app.get('/help')             //app.com/help
+app.get('*', (req, res) => {
+    res.render('404', {
+        error: 'Page not found',
+        title: '404',
+        name: 'Frank Fourlas'
+    })
+})
 
 app.listen(3000, () => {
     console.log("Server running on port 3000.")
